9ded23c25c8e6adae379855288a835fc679e3571
Michelzao <vildo.michel@gmail.com>
2023-10-29 02:36:03 -0300

    update no form

--- Estacionamento.UI/src/app/components/entrada/entrada.component.ts
+++ Estacionamento.UI/src/app/components/entrada/entrada.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'carro-entrada',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './entrada.component.html',
   styleUrls: ['./entrada.component.scss']
 })
-export class EntradaComponent {
 
+export class EntradaComponent {
+  carroForm!: FormGroup;
+  private formBuilder = inject(FormBuilder);
+  private route = inject(Router);
+  constructor()
+  {
+    this.createForm();
+  }
+
+  createForm(): void{
+    this.carroForm = this.formBuilder.group({
+      Marca: ["", Validators.required],
+      Modelo: ["", Validators.required],
+      Placa: ["", Validators.required],
+      HoraEntrada: ["", Validators.required],
+    });
+  }
+
+  onSubmit(){
+    const formValues = this.carroForm.value;
+    console.log(formValues);
+    this.route.navigate(['']);
+  }
 }
