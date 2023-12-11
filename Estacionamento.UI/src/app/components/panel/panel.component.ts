d00d7572ecc6d8d5dcb1568b92f82b8235ddb37e
MichelOB <vildo.michel@gmail.com>
2023-12-10 21:58:04 -0300

    alteracoes na viagem

--- Estacionamento.UI/src/app/components/panel/panel.component.ts
+++ Estacionamento.UI/src/app/components/panel/panel.component.ts
@@ -10,4 +10,22 @@ import { CommonModule } from '@angular/common';
 })
 export class PanelComponent {
 
+  spotList: any[] = []
+
+  constructor(){
+    this.createSpots();
+  }
+
+  createSpots()
+  {
+    this.spotList = [
+      {id: 1, isOccuped: false, currentCar: {Marca: 'FIAT', Modelo: 'Sienao', Placa: 'LOD7452', }},
+      {id: 2, isOccuped: false},
+      {id: 3, isOccuped: false},
+      {id: 4, isOccuped: false},
+      
+    ]
+
+  }
+
 }
